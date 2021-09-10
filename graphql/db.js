7ea7cd5218b4283ecd154fcab078ba805e80516b
JUNGganzi <wjdwjd1501@gmail.com>
2021-09-11 01:07:23 +0900

    mutation 사용방법

--- graphql/db.js
+++ graphql/db.js
@@ -1,31 +1,51 @@
+
+export let movies = [
+    {
+        id :1,
+        name : "jungwon",
+        score : 0.1
+    },
+    {
+        id :2,
+        name : "jungwon",
+        score : 5
+    },
+    {
+        id :3,
+        name : "jungwon",
+        score : 0.2
+    },
+    {
+        id :4,
+        name : "jungwon",
+        score : 4
+    },
+]
+
+export const getMovies = () => movies
+
 export const getById = id => {
-    const filteredPeople = people.filter(person => person.id === String(id))
-    return filteredPeople[0]
+    const filteredMovie = movies.filter(movie => movie.id === String(id))
+    return filteredMovie[0]
 }
 
-export const people = [
-    {
-    id :"1",
-    name : "jungwon",
-    age : 29,
-    gender : "male"
-},
-{
-    id :"2",
-    name : "wonjung",
-    age : 123,
-    gender : "male"
-},
-{
-    id :"3",
-    name : "12314",
-    age : 123,
-    gender : "male"
-},
-{
-    id :"4",
-    name : "12314",
-    age : 234,
-    gender : "male"
-},
-]
+export const deleteMovie = (id) => {
+    const cleanedMovies = movies.filter(movie => movie.id !== String(id))
+    if(movies.length > cleanedMovies.length) {
+        movies = cleanedMovies
+        return true
+    } else {
+        return false
+    }
+}
+
+export const addMovie = (name, score) => {
+    const newMovie = {
+        id : `${movies.length + 1}`,
+        name,
+        score
+    }
+    movies.push(newMovie)
+    return newMovie
+}
+
